882d08a4e97fae2e0dbb1d49b10ca805b0334dd5
戴亮 <dailiang@hujiang.com>
2016-01-07 14:26:34 +0800

    fix the issue

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var htmlclean = require("htmlclean");
 
 module.exports = function(source) {
-  var callback = this.async();
   if (this.cacheable) {
     this.cacheable();
   }
   var opts = this.options['html-clean-loader'] || {};
-  var html = htmlclean(source, options);
-  callback(html);
+  return htmlclean(source, opts);
 }
